b9d6a8100b73803198fe282eaee53a69d579fd94
Joaquin Gonzalez V <villaescusareview@gmail.com>
2022-09-21 14:02:44 -0400

    Update version & add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import {InputTextareaModule} from 'primeng/inputtextarea';
 import {ToastModule} from 'primeng/toast';
 
 import { DndModule } from 'src/app/components/dnd/dnd.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -57,7 +59,13 @@ import { DndModule } from 'src/app/components/dnd/dnd.module';
     DndModule,
     CalendarModule,
     InputTextareaModule,
-    ToastModule
+    ToastModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
